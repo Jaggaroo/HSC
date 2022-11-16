649b34683be561f6a93164cc947d924c2279fcbf
John Green <38727877+Jaggaroo@users.noreply.github.com>
2022-11-16 19:44:41 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// Button Toggle function
+
+
+// function toggleabmn() {
+//   let abmn = document.querySelector(".albertamanitoba");
+//   abmn.style.display = "none";
+// }
+
+
+
+
+// const abmn = document.getElementById("albertamanitoba");
+// const onqc = document.getElementById("ontarioquebec");
+// const nb = document.getElementById("newbrunswick");
+// const ns = document.getElementById("novascotia");
+
+// Alberta and Manitoba
+function toggleabmn() {
+  let abmn = document.getElementById(#albertamanitoba);
+  if (abmn.style.display = "none";) {
+    abmn.style.display = "block";
+  } else {
+      abmn.style.display = "none";
+  }
+}
+
+let abmn = document.querySelector(".albertamanitoba");
+ function toggleabmn() {
+   abmn.classList.toggle("hide");
+ }
+
+
+// Ontario and Quebec
+// toggleonqc.addEvntListener('click', function handleClick()) {
+//   // if (onqc.style.display === 'none') {
+//     abmn.style.display = 'none';
+//     // onqc.style.display = 'block'}
+// }
+
+// New Brunswick
+function togglenb() {
+
+}
+
+// Nova Scotia
+function togglens() {
+
+}
+
+// Newfoundland & Labrador
+function togglenl() {
+
+}
+
+// Prine Edward Island
+function togglepe() {
+
+}
